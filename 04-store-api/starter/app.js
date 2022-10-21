5b20ec82061b33717bd3468051f0997406d0b226
Toni Uimonen <tauimonen@gmail.com>
2022-10-21 15:41:24 +0300

    Create product schema

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -4,6 +4,9 @@ require('dotenv').config()
 const express = require('express')
 const app = express();
 
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
+
 const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
 
@@ -15,3 +18,24 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
 })
+
+app.use('/api/v1/products', productsRouter)
+
+// products route
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try {
+        // connect database
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening port ${port}...`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
